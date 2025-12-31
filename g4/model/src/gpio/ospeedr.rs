a8c6ae5fa7f382cd41b34aec4a765df3b9fd8f52
Adin Ackerman <adin@fourier.earth>
2025-12-30 19:13:41 -0800

    g4: fix gpio

--- g4/model/src/gpio/ospeedr.rs
+++ g4/model/src/gpio/ospeedr.rs
@@ -6,8 +6,7 @@ use crate::gpio::{Instance, ospeedr::ospeed::ospeed};
 
 pub fn ospeedr<'cx>(gpio: &mut PeripheralEntry<'cx>, instance: Instance) {
     let mut ospeedr = gpio.add_register(Register::new("ospeedr", 8).reset(match instance {
-        Instance::A => 0x08,
-        Instance::B => 0x0c00_0000,
+        Instance::A => 0x0c00_0000,
         _ => 0,
     }));
 
