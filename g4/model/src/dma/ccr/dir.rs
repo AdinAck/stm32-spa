4f47125f1ef69d3ef94f8eb4e9913d6e60b573e5
AdinAck <adinackerman@gmail.com>
2026-03-18 09:33:10 -0700

    add dma peripherals (#5)

---
+++ g4/model/src/dma/ccr/dir.rs
@@ -0,0 +1,14 @@
+use phm::{Field, Variant, model::RegisterEntry};
+
+use crate::dma::ccr::en;
+
+pub fn dir<'cx>(ccr: &mut RegisterEntry<'cx>, en: en::Output) -> phm::Result<()> {
+    let mut dir = ccr.add_store_field(Field::new("dir", 4, 1).docs(["Data transfer direction"]));
+
+    dir.write_entitlements([[en.disabled]])?;
+
+    dir.add_variant(Variant::new("ReadFromPeripheral", 0));
+    dir.add_variant(Variant::new("ReadFromMemory", 1));
+
+    Ok(())
+}
---
+++ g4/model/src/dma/ccr/teie.rs
@@ -0,0 +1,15 @@
+use phm::{Field, Variant, model::RegisterEntry};
+
+use crate::dma::ccr::en;
+
+pub fn teie<'cx>(ccr: &mut RegisterEntry<'cx>, en: en::Output) -> phm::Result<()> {
+    let mut teie =
+        ccr.add_store_field(Field::new("teie", 3, 1).docs(["Transfer error interrupt enable"]));
+
+    teie.write_entitlements([[en.disabled]])?;
+
+    teie.add_variant(Variant::new("Disabled", 0));
+    teie.add_variant(Variant::new("Enabled", 1));
+
+    Ok(())
+}
