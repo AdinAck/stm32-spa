03772e82608ef329b22d4ba8913ea3919ff4c739
AdinAck <adinackerman@gmail.com>
2026-05-14 17:32:12 -0700

    vrefbuf: vrs write entitlements (#24)

--- model/src/devices/g4/vrefbuf/csr/vrs.rs
+++ model/src/devices/g4/vrefbuf/csr/vrs.rs
@@ -1,7 +1,11 @@
 use phm::{Field, Variant, model::RegisterEntry};
 
-pub fn vrs<'cx>(csr: &mut RegisterEntry<'cx>) {
+use crate::devices::g4::vrefbuf::csr::envr;
+
+pub fn vrs<'cx>(csr: &mut RegisterEntry<'cx>, envr: &envr::Output) {
     let mut vrs = csr.add_store_field(Field::new("vrs", 4, 2));
+
+    vrs.write_entitlements([[envr.disabled]]);
 
     vrs.add_variant(Variant::new("R2V048", 0).docs(["2.048v."]));
     vrs.add_variant(Variant::new("R2V500", 1).docs(["2.500v"]));
