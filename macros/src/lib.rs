81cccc69fc3a3cdd4ec218d5fdf53f99326aedb8
AdinAck <adinackerman@gmail.com>
2026-06-06 11:44:57 -0700

    composition mode (#26)
    Introduce a model composition mode to separate between composing for
    *validation* and *production*.
    
    For example, the model can include the appropriate Cortex-M* model such
    that physical validation occurs, but omit the cortex model during
    production so that the gates do not have access to that model (overlap).

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,3 +1,4 @@
+use model::Mode;
 #[allow(unused)]
 use model::{
     Device,
@@ -5,13 +6,16 @@ use model::{
 };
 use proto_hal_macros::generate_macros;
 
-generate_macros!({
-    cfg_select! {
-        feature = "c092" => Some(Device::C0(c0::Variant::C092)),
-        feature = "g431" => Some(Device::G4(g4::Variant::G431)),
-        feature = "g441" => Some(Device::G4(g4::Variant::G441)),
-        feature = "g474" => Some(Device::G4(g4::Variant::G474)),
-        feature = "g484" => Some(Device::G4(g4::Variant::G484)),
-        _ => None,
-    }
-});
+generate_macros!(
+    {
+        cfg_select! {
+            feature = "c092" => Some(Device::C0(c0::Variant::C092)),
+            feature = "g431" => Some(Device::G4(g4::Variant::G431)),
+            feature = "g441" => Some(Device::G4(g4::Variant::G441)),
+            feature = "g474" => Some(Device::G4(g4::Variant::G474)),
+            feature = "g484" => Some(Device::G4(g4::Variant::G484)),
+            _ => None,
+        }
+    },
+    Mode::Production,
+);
--- model/src/main.rs
+++ model/src/main.rs
@@ -1,14 +1,14 @@
 use std::process::ExitCode;
 
 use enum_iterator::all;
-use stm32_spa_model::compose;
+use stm32_spa_model::{Mode, compose};
 
 fn main() -> ExitCode {
     let mut exit_code = ExitCode::SUCCESS;
 
     for variant in all() {
         println!("=== Variant: {variant:?} ===");
-        if ExitCode::FAILURE == phm::validate(compose(Some(variant))) {
+        if ExitCode::FAILURE == phm::validate(compose(Some(variant), Mode::Validation)) {
             exit_code = ExitCode::FAILURE
         }
     }
